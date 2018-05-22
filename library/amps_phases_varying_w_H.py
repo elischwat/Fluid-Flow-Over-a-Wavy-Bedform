c9b9c0b16a4242ec78c4a3961564f5ee1f1b940c
Eli Schwat <eli.l.schwat@gmail.com>
2018-05-21 21:08:01 -0400

    Added section about varying H.

--- library/amps_phases_varying_w_H.py
+++ library/amps_phases_varying_w_H.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr  3 00:56:09 2018
+Created on Wed Apr  4 18:27:43 2018
 
 @author: elischwat
 """
@@ -12,10 +12,11 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.ticker as ticker
 
-def plotAmpsPhasesVaryingWithU(eta0, lamb, U, g, H, gamma):
 
-  UArr = U
+def plotAmpsPhasesVaryingWithH(eta0, lamb, U, g, H, gamma):
 
+  HArr = H
+  
   def zNaught(eta0, lamb, U, g, H, gamma):
       alpha = gamma/H; beta = g*H/U**2; omega = 2*np.pi/lamb
       return eta0*(9*alpha**2 + omega**2)/np.sqrt(9*alpha**2 + omega**2*(1-beta)**2)
@@ -33,42 +34,39 @@ def plotAmpsPhasesVaryingWithU(eta0, lamb, U, g, H, gamma):
       return np.arctan(omega/(3*alpha)) - np.arctan(omega*(1-beta)/(3*alpha))    
 
   plt.subplot(2, 2, 1)
-  plt.plot(UArr, zNaught(eta0, lamb, UArr, g, H, gamma))
-  plt.xlim(0), plt.ylim(0)
+  plt.plot(HArr, zNaught(eta0, lamb, U, g, HArr, gamma))
   ax = plt.gca()
-  # ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f' + 'm'))
-  ax.text(.6,.9,'$\zeta_0$, Fluid Surface Amplitude',
+  ax.yaxis.set_major_formatter(FormatStrFormatter('%.00f' + 'm'))
+  ax.text(.5,.9,'$\zeta_0$, Fluid Surface Amplitude',
           horizontalalignment='center',
           transform=ax.transAxes, fontsize=15)
 
   plt.subplot(2, 2, 2)
-  plt.plot(UArr, zPhase(eta0, lamb, UArr, g, H, gamma))
-  #plt.xlim(0), plt.ylim(0)
+  plt.plot(HArr, zPhase(eta0, lamb, U, g, HArr, gamma))
   labels = [r'$0$', r'$\frac{1}{2} \pi$', r'$\pi$']
   plt.yticks([0, 1.57075, 3.1415],labels, fontsize=14)
   ax = plt.gca()
-  ax.text(.6,.9,'$\phi_\zeta$, Fluid Surface Phase',
+  ax.text(.5,.8,'$\phi_\zeta$, Fluid Surface Phase',
           horizontalalignment='center',
           transform=ax.transAxes, fontsize=15)
 
-
   plt.subplot(2, 2, 3)
-  plt.plot(UArr, uNaught(eta0, lamb, UArr, g, H, gamma))
-  plt.xlabel('U, Reach-Averaged Fluid Velocity')
-  plt.xlim(0), plt.ylim(0)
+  plt.plot(HArr, uNaught(eta0, lamb, U, g, HArr, gamma))
+  plt.xlabel('H, Reach-Averaged Fluid Depth')
   ax = plt.gca()
   ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f' + 'm'))
-  ax.text(.6,.9,'$u_0$, Velocity Amplitude',
+  ax.text(.5,.9,'$u_0$, Velocity Amplitude',
           horizontalalignment='center',
           transform=ax.transAxes, fontsize=15)
 
   plt.subplot(2, 2, 4)
-  plt.plot(UArr, uPhase(eta0, lamb, UArr, g, H, gamma))
-  plt.xlabel('U, Reach-Averaged Fluid Velocity')
-  labels = [r'$0$', r'-$\frac{1}{2} \pi$', r'-$\pi$']
-  plt.yticks([0, -1.57075, -3.1415],labels, fontsize=14)
-  ax = plt.gca()
-  ax.text(.6,.9,'$\phi_u$, Velocity Phase',
+  plt.plot(HArr, uPhase(eta0, lamb, U, g, HArr, gamma))
+  plt.xlabel('H, Reach-Averaged Fluid Depth')
+
+  labels = [r'$0$', r'$-\frac{1}{2} \pi$']
+  plt.yticks([0, -1.57075],labels, fontsize=14)
+  ax= plt.gca()
+  ax.text(.5,.8,'$\phi_u$, Velocity Phase',
           horizontalalignment='center',
           transform=ax.transAxes, fontsize=15)
 
